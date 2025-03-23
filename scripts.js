6b760f3e838a9716aca2cfe24deb85ebd49df113
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-03-23 09:20:07 +0200

    Added apply theme logic from previous rendition
    Added apply theme logic from previous rendition

--- scripts.js
+++ scripts.js
@@ -362,6 +362,17 @@ loadTheme() {
 document.querySelector("[data-list-close]").addEventListener("click",()=>{
     toggleModal("[data-list-active]", false);
 });
+applyTheme(theme) {
+    if (theme === "night") {
+        document.documentElement.style.setProperty("--color-dark", "255, 255, 255");
+        document.documentElement.style.setProperty("--color-light", "10, 10, 20");
+    } else {
+        document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
+        document.documentElement.style.setProperty("--color-light", "255, 255, 255");
+    }
+}
+
+}
 
 document.querySelector("[data-list-button]").addEventListener("click", loadMoreBooks);
 
